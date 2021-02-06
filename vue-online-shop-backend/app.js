5c0ff964ee11b7c855b47d4f2a4f7d215f4a4675
mukiwu <mukispace@gmail.com>
2021-02-06 15:19:42 +0800

    cross domain

--- vue-online-shop-backend/app.js
+++ vue-online-shop-backend/app.js
@@ -17,6 +17,20 @@ app.set('view engine', 'ejs');
 // Database connection here
 mongoose.connect(`mongodb://localhost:27017/test`);
 
+// CORS config here
+app.all('/*', function(req, res, next) {
+  // CORS headers
+  res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  // Set custom headers for CORS
+  res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key');
+  if (req.method == 'OPTIONS') {
+    res.status(200).end();
+  } else {
+    next();
+  }
+});
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
