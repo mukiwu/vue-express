950db917734d9f2900c287f2ef9673b8790befe0
mukiwu <mukispace@gmail.com>
2021-02-06 15:22:53 +0800

    導入 api 路由

--- vue-online-shop-backend/app.js
+++ vue-online-shop-backend/app.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const api = require('./routes/api');
 
 var app = express();
 
@@ -39,6 +40,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api/v1', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -55,7 +57,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
-app.use(logger('dev'));
 
 module.exports = app;
